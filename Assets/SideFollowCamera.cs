732e97fbf8479ddc75a8135dd4a5aa8470e516e6
amphy0 <106935682+amphy0@users.noreply.github.com>
2024-11-01 17:34:35 -0700

    fixcamera
    fix camera values

--- Assets/SideFollowCamera.cs
+++ Assets/SideFollowCamera.cs
@@ -18,7 +18,5 @@ public class SideFollowCamera : MonoBehaviour
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
 
         transform.position = smoothedPosition;
-
-        transform.LookAt(player);
     }
 }
